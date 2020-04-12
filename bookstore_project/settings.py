7cca72ed9f218fd6a2a11fcb1e7c416892f826d7
Collin <ococncol@gmail.com>
2020-04-12 16:06:37 -0400

    ch6

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party,
+    'crispy_forms',
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig'
@@ -125,8 +127,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- pages/tests.py
+++ pages/tests.py
@@ -1,10 +1,10 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
-class HomePageTest(SimpleTestCase):
+class HomePageTests(SimpleTestCase):
 
     def setUp(self):
         url = reverse('home')
@@ -27,4 +27,30 @@ class HomePageTest(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             HomePageView.as_view().__name__
+        )
+
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response,
+            'Hi I should not be on this page'
+        )
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
         )
