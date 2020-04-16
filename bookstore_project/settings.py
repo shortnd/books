69a0176146fe26d72678e5a42559f91e585898d2
Collin OConnell <ococncol@gmail.com>
2020-04-16 12:37:56 -0400

    allowed hosts

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['thawing-journey-15781.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
